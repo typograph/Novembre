b862654e1e2359b741d1db8fff700c327b374c3d
Tim Fuchs <typograph@users.noreply.github.com>
2011-03-22 19:44:05 +0100

    Update available comment list [work in progress]

--- src/createc.cpp
+++ src/createc.cpp
@@ -35,13 +35,14 @@ QStringList CreatecFileGenerator::availableInfoFields() const {
     return QStringList() \
             << "Bias" \
             << "Setpoint" \
-						<< "VertFBMode" \
+						<< "Spectroscopy feedback mode" \
 						<< "Scanmode" \
 						<< "CHMode" \
 						<< "VFBMode" \
 						<< "RotCMode" \
 						<< "Latmanmode" \
 						<< "LockinMode" \
+						<< "z(V) setpoint current" \
             ;
 }
 
@@ -223,15 +224,196 @@ NVBFileInfo * CreatecFileGenerator::loadFileInfo( const NVBAssociatedFilesInfo &
 	//  QSize size;
 	QMap<QString,NVBVariant> comments;
 	
-	comments.insert("Bias",header.value("BiasVoltage").toPhysValue());
-	comments.insert("Setpoint",header.value("Current").toPhysValue());
-	if (type == NVB::SpecPage) comments.insert("VertFBMode",header.value("VertFBMode").toInt());
-	comments.insert("Scanmode",header.value("Scanmode").toInt());
 	comments.insert("CHMode",header.value("CHMode").toInt());
 	comments.insert("VFBMode",header.value("VFBMode").toInt());
 	comments.insert("RotCMode",header.value("RotCMode").toInt());
 	if (type == NVB::SpecPage) comments.insert("Latmanmode",header.value("Latmanmode").toInt());
 	comments.insert("LockinMode",header.value("LockinMode").toInt());
+
+	comments.insert("DAC resolution",NVBPhysValue(header.value("DAC-Type").toString(),false));
+	//	Titel=default            //----- Never saw that set. Theoretically could be used to name pages
+	//	Delta X = 512            //-[]--- Dac step in X
+	//	Delta Y = 512            //-[]--- Dac step in Y
+	//	Num.X = 256              //-[]--- Number of points in X
+	//	Num.Y = 256              //-[]--- Number of points in Y
+	//	Delay X+ = 1             //--[]-- ? Some time parameter
+	//	Delay X- = 1             //--[]-- ? Some time parameter
+	//	Delay Y  = 1             //--[]-- ? Some time parameter
+	//	D-DeltaX = 8             //--[]-- X step size
+	//	GainX = 10	           	 //-[]--- X Gain
+	//	GainY = 10               //-[]--- Y Gain
+	//	GainZ = 3                //-[]--- Z Gain
+	//	Rotation = 0             //--[]-- Rotation angle
+	comments.insert("Bias",header.value("BiasVoltage").toPhysValue());
+	comments.insert("Amplifier setting",header.value("Gainpreamp").toInt());
+	//	Chan(1,2,4) = 1          //-[]--- Channel number. Already used in number of pages
+	//	PlanDx = 0.0000          //----- X Plane subtract param. Not very useful
+	//	PlanDy = 0.0000          //----- X Plane subtract param. Not very useful
+	//	Scanrotoffx = -99611.7   //-[]--- X Offset
+	//	Scanrotoffy = -225437.0  //-[]--- Y Offset
+	//	MVolt_1 = 0	           	 //====== Multi-voltage measurement. (has to be treated somehow)
+	//	MVolt_2 = 0	           	 //====== Multi-voltage measurement. (has to be treated somehow)
+	//	MVolt_3 = 0	           	 //====== Multi-voltage measurement. (has to be treated somehow)
+	//	MVolt_4 = 0	           	 //====== Multi-voltage measurement. (has to be treated somehow)
+	//	MVolt_5 = 0	           	 //====== Multi-voltage measurement. (has to be treated somehow)
+	//	MVolt_6 = 0	           	 //====== Multi-voltage measurement. (has to be treated somehow)
+	//	RepeatRotinc = 0
+	//	RptBVoltinc = 0
+	//	Repeatinterval = 0
+	//	Repeatcounter = 0
+	//	RepeatXoffset = 0
+	//	RepeatYoffset = 0
+	//	Scantype = 1
+	//	Scanmode = 2 //---- 1 means forward, 2 meand forward + backward
+	//	Scancoarse = 1
+	//	CHMode = 0
+	//	Channels = 2
+	//	Preamptype = 1
+	//	VFBMode = 0
+	//	RotCMode = 0
+	//	CHModeZoff = 0
+	//	CHModeGainpreamp = 6
+	//	Latmanmode = 1
+	//	LatmResist = 1.000E+00
+	//	LatmanVolt = 1000
+	//	Latmanlgi = 0
+	//	Latmangain = 8
+	//	Latmanddx = 32
+	//	Latmandelay = 96
+	//	Latm0Delay = 2
+	//	Latmanccdz = 0
+	//	Latmanextension = 1.000
+	//	TipForm_Volt = -3000
+	//	TipForm_Z = 100
+	//	Tip_Delay = 100
+	//	Tip_Latddx = 32
+	//	Tip_LatDelay = 102
+	//	Tip_Gain = 6
+	//	Vertmangain = 8
+	//	Vertmandelay = 10
+	//	Vpoint0.t = 0
+	//	Vpoint1.t = 400
+	//	Vpoint2.t = 600
+	//	Vpoint3.t = 1024
+	//	Vpoint0.V = 0
+	//	Vpoint1.V = -1000
+	//	Vpoint2.V = -1000
+	//	Vpoint3.V = 0
+	//	Zpoint0.t = 0
+	//	Zpoint1.t = 400
+	//	Zpoint2.t = 600
+	//	Zpoint3.t = 1024
+	//	Zpoint0.z = 0
+	//	Zpoint1.z = -100
+	//	Zpoint2.z = -100
+	//	Zpoint3.z = 0
+	//	Zoffset = 0
+	//	Zdrift = 0
+	//	VertSpecBack = 0
+	//	VertSpecAvrgnr = 1
+	//	VertRepeatCounter = 1
+	//	VertLineCount = 2
+	//	SpecXGrid = 256
+	//	SpecYGrid = 100000
+	//	SpecXYGridDelay = 10
+	//	SpecGridChan = 2
+	//	VerttreshImax = 20000
+	//	VerttreshImin = -20000
+	//	VertAvrgdelay = 100
+	if (type == NVB::SpecPage)
+		switch(header.value("VertFBMode").toInt()) {
+			case 4:
+				comments.insert("Spectroscopy feedback mode",QString("I(V,z)"));
+				break;
+			case 1:
+				comments.insert("Spectroscopy feedback mode",QString("z(V)"));
+				break;
+			default:
+				NVBOutputError(QString("Unknown VertFBMode value: %1").arg(header.value("VertFBMode").toInt()));
+			}
+	comments.insert("z(V) setpoint current",NVBPhysValue(header.value("VertFBLogiset").toDouble()*exp10(-header.value("Vertmangain").toInt()),NVBDimension("mA")));
+	//	Imageframe = 0
+	//	Imagegrayfactor = 32.416
+	//	Zoom = 2.00
+	//	OrgPlanX = 0.00000
+	//	OrgPlanY = 0.00000
+	//	OrgPlanOff = 0.00000
+	//	Dacto[A]xy = 0.00191
+	//	Dacto[A]z = 0.00038
+	//	Planoff = 0
+	//	Planx = 0
+	//	Plany = 0
+	//	Plany2 = 0
+	//	Planavrgnr = 17
+	//	Planoffset2 = 0
+	//	Plano2start = 0
+	//	Imagebackoffset = 0
+	//	Frameimageoffset = 1000
+	//	DigZoomX = 1.00000
+	//	DigZoomZ = 1.00000
+	//	FBLogIset = 255.100
+	//	FBIntegral = -28.481974
+	//	FBProp = 0.000000
+	//	FBVoltRC = 0.000100
+	//	FBVoltGain = 10000.000
+	//	CurrentRC = 0.9000
+	//	FBLog = 1
+	//	Imaxcurrent = 1000000.0
+	//	Imaxdelay = 1
+	//	ImaxZret = 0.000
+	//	SpecAvrgnr = 32
+	//	SpecFreq = 10000.000
+	//	FFTPoints = 1024
+	//	LockinFreq = 100
+	//	LockinAmpl = 100
+	//	LockinPhase = 0
+	//	LockinPhase2 = 0
+	//	LockinRC = 0.00100
+	//	LockinMode = 0
+	//	SBC_Clk[MHz] = 150
+	//	DSP_Clock = 50000
+	//	SRS_Frequency = 1000.000
+	//	SRS_ModVoltage = 1.0000
+	//	SRS_InpGain[V] = 1.0E+00
+	//	SRS_InpTimeC[s] = 1.0E+00
+	//	LockinRefAmpl = 0
+	//	LockinRefPhase = 0
+	//	DAC5[V]=0.00000
+	//	UserPreampCode = 6:011000/8:101000/10:110000/12:111000/
+	//	HPIB_Address = hpib7,9
+	//	Upinc = 20164
+	//	Upcount = 13
+	//	Rotinc = 20164
+	//	Rotincquad = 0
+	//	Rotcount = 13
+	//	XYBurst = 1000
+	//	RotBurst = 1000
+	//	AproBurst = 2
+	//	AproPeriod = 300
+	//	X-Puls-Count = -9300
+	//	Y-Puls-Count = -12240
+	//	Z-Puls-Count = -2988699
+	//	Autolevel = 0
+	//	Scandvinc[DAC] = 0
+	//	Z-Res. [A]: +/- =4.749
+	//	Length x[A] = 2500.0763
+	//	Length y[A] = 2500.0763
+	//	Biasvolt[mV] = 511 //---- Duplicate of BiasVoltage
+	comments.insert("Setpoint",header.value("Current").toPhysValue());
+	//	Sec/line: = 0.328
+	//	Sec/Image: = 167.772
+	//	ActGainXYZ = 10 10 3
+	//	Channels = 2
+	//	T-STM: = 0.00000
+	//	HP_Ch1 = 0.00000
+	//	HP_Ch2 = 0.00000
+	//	HP_Ch3 = 0.00000
+	//	HP_Ch4 = 0.00000
+	//	ZPiezoconst = 20.00
+	//	Xpiezoconst = 100.00
+	//	YPiezoconst = 100.00
+	//	T_ADC2[K]=  0.000
+	//	T_ADC3[K]=  0.000
 
 	int nchannels = header.value("Channels").toInt();
 #ifdef TMP_VOLTAGE_MAP
@@ -399,7 +581,15 @@ CreatecDatPage::CreatecDatPage( CreatecHeader file_header, int channel, double *
       break;
     }
   }
-      
+
+	comments.insert("Bias",header.value("BiasVoltage").toPhysValue());
+	comments.insert("Setpoint",header.value("Current").toPhysValue());
+	comments.insert("Scanmode",header.value("Scanmode").toInt());
+	comments.insert("CHMode",header.value("CHMode").toInt());
+	comments.insert("VFBMode",header.value("VFBMode").toInt());
+	comments.insert("RotCMode",header.value("RotCMode").toInt());
+	comments.insert("LockinMode",header.value("LockinMode").toInt());
+
   getMinMax();
 
   // colors
@@ -559,7 +749,7 @@ QList<NVBDataSource*> CreatecVertPage::loadAllChannels(QStringList filenames) {
 		double dacFactor = file_header.value("Dacto[A]xy").toDouble()*1e-9;
 		QPointF pos = QPointF(Xdac*dacFactor,Ydac*dacFactor);
 
-		NVBOutputDMsg(QString("X: %1, Y: %2 : DAC %3 -> Point %4 x %5").arg(Xdac).arg(Ydac).arg(dacFactor,8).arg(pos.x(),8).arg(pos.y(),8));
+//		NVBOutputDMsg(QString("X: %1, Y: %2 : DAC %3 -> Point %4 x %5").arg(Xdac).arg(Ydac).arg(dacFactor,8).arg(pos.x(),8).arg(pos.y(),8));
 
 //	int nchannels = file_header.value("Channels").toInt();
 //  int data_points = file_header.value("Num.X",0).toInt() * file_header.value("Num.Y",0).toInt();
