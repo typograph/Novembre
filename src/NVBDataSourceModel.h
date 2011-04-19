b1a8dda688d52017d4ee58c8a123b1cb3bbdaa86
Timofey Balashov <typograph@elec.ru>
2011-04-19 11:01:43 +0200

    Created NVBDataSourceList model for showing entire files

--- src/NVBDataSourceModel.h
+++ src/NVBDataSourceModel.h
@@ -48,29 +48,36 @@ public:
 //  virtual bool removeRow ( int row, const QModelIndex & parent = QModelIndex() );
 
   virtual QMimeData * mimeData ( const QModelIndexList & indexes ) const;
+  virtual QMimeData * mimeData ( const QModelIndex & index ) const;
 //  virtual Qt::DropActions supportedDropActions () const;
   virtual QStringList mimeTypes () const;
 //  virtual bool dropMimeData ( const QMimeData * data, Qt::DropAction action, int row, int column, const QModelIndex & parent );
-
-//  virtual NVB::ViewType viewType() { return NVB::IconView; }
-
-//  virtual void setSource(NVBDataSource * page, NVBVizUnion viz = NVBVizUnion());
-//  virtual void addSource(NVBDataSource * page, NVBVizUnion viz = NVBVizUnion());
-//  void addSource(NVBDataSource * page, int row, NVBVizUnion viz = NVBVizUnion());
-//  void addSource(const QModelIndex & index);
-
-//  virtual void setVisualizer(NVBVizUnion visualizer);
-//  virtual void addControlWidget(QWidget * controlWidget);
-//  virtual void setActiveVisualizer(NVBVizUnion visualizer);
-
-//  virtual NVBViewController * openInNewWindow(NVBDataSource * page, NVBVizUnion viz = NVBVizUnion(), NVB::ViewType vtype = NVB::DefaultView);
-
-// public slots :
-//   void updateIcons(const QModelIndex & tl, const QModelIndex & br);
-//  virtual void refresh();
-
+/*
 protected slots:
-//  virtual void updateSource( NVBDataSource * newobj, NVBDataSource * oldobj);
+	void dataSourceChanged();
+	void dataSourceReplaced( NVBDataSource * newobj, NVBDataSource * oldobj);
+	*/
+};
+
+class NVBDataSourceListModelPrivate;
+
+class NVBDataSourceListModel : public QAbstractListModel {
+
+private:
+	NVBDataSourceListModelPrivate * p;
+
+public:
+	explicit NVBDataSourceListModel(QList< NVBDataSource * > sources);
+	virtual ~NVBDataSourceListModel();
+
+  virtual int rowCount ( const QModelIndex & parent = QModelIndex() ) const;
+  virtual QVariant data ( const QModelIndex & index, int role = Qt::DisplayRole ) const;
+
+  virtual Qt::ItemFlags flags ( const QModelIndex & index ) const;
+
+  virtual QMimeData * mimeData ( const QModelIndexList & indexes ) const;
+  virtual QStringList mimeTypes () const;
+
 };
 
 #endif
