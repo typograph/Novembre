c67b1ebaece4302f716ec92cc7bc9fd0237e3efc
Timofey Balashov <typograph@elec.ru>
2011-04-24 01:43:44 +0200

    testGenerator now lets you select the file to show

--- src/testGenerator/testGenerator.h
+++ src/testGenerator/testGenerator.h
@@ -4,19 +4,30 @@
 #include <QtGui/QApplication>
 #include <QtCore/QEvent>
 #include <QtCore/QObject>
+#include <QtCore/QString>
 #include "../NVBLogger.h"
+
+class QLineEdit;
+class QTreeWidget;
+class QListView;
 
 class NVBTestGenApplication : public QApplication {
 Q_OBJECT
 private:
 public:
-  NVBTestGenApplication ( int & argc, char ** argv );
-  virtual ~NVBTestGenApplication();
-  virtual bool notify ( QObject * receiver, QEvent * event ) ;
+	NVBTestGenApplication ( int & argc, char ** argv );
+	virtual ~NVBTestGenApplication();
+	virtual bool notify ( QObject * receiver, QEvent * event ) ;
 
+	QLineEdit * files;
+	QTreeWidget * tree;
+	QListView * view;
+
+public slots:
+	void openFile(QString = QString());
 #ifdef NVB_ENABLE_LOG
 private slots:
-  void message(NVB::LogEntryType type, QString issuer, QString text);
+	void message(NVB::LogEntryType type, QString issuer, QString text);
 #endif
 };
 
