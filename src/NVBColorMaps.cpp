388169f9649a9a9475ecedc75bf186be17b6f8a8
tbalashov.icn <tbalashov.icn@localhost>
2011-08-19 23:04:41 +0200

    Added grey step gradient map and an RGBMix max with 3 separate color channel gradients

--- src/NVBColorMaps.cpp
+++ src/NVBColorMaps.cpp
@@ -80,3 +80,29 @@ NVBRGBRampColorMap::NVBRGBRampColorMap(double r_min, double r_max, double g_min,
   quint32 rgb_max = 0xFF000000 + (((uchar)(r_max*0xFF)) << 16) + (((uchar)(g_max*0xFF)) << 8) + (uchar)(b_max*0xFF);
 	rgb = NVBValueScaler<double,quint32>(0,1,rgb_min,rgb_max);
 }
+
+void NVBGrayStepColorMap::addStep(double x, double value) {
+	QList<double>::iterator i = qLowerBound(steps.begin(),steps.end(),x);
+	int index = i-steps.begin()-1;
+	steps.insert(i,x);
+
+	NVBValueScaler<double,double> old = scales.takeAt(index);
+	scales.insert(index,
+								NVBValueScaler<double,double>(
+									steps.at(index),x,
+									old.scale(steps.at(index)),value)
+								);
+	scales.insert(index+1,
+								NVBValueScaler<double,double>(
+									x,steps.at(index+1),
+									value,old.scale(steps.at(index+1)))
+								);
+}
+
+QRgb NVBGrayStepColorMap::colorize(double z) const {
+	int index = qLowerBound(steps.begin(),steps.end(),z) - steps.begin() - 1;
+	if (index == -1) index = 0;
+
+	quint32 u = 0xFF & (uchar)(0xFF*( scales.at(index).scale(z)));
+	return (quint32)(0xFF000000 | (u << 16) | (u << 8) | u);
+}
